87983be268d1c0d7ab73c1b57b22c38c00986aea
argabas <argabas@outlook.com>
2023-07-05 09:25:17 -0300

    correcao

--- test/specs/start.spec.js
+++ test/specs/start.spec.js
@@ -2,11 +2,11 @@ describe('Primeiro teste', () => {
     it('acessar o menu forms', async () => {
         await $('~Forms').click()
         await $('~text-input').setValue('Alexandre')
-        await $('android.widget.EditText').click()
+        await $('android=new UiSelector().text("Select an item...")').click()
         await $('android=new UiSelector().text("webdriver.io is awesome")').click()
         await $('~button-Active').click()
-
-       expect (await $('android.widget.LinearLayout').toBeDisplayed())
         
+        expect(await $('android.widget.LinearLayout').isDisplayed()).toBeTruthy()
+            
     });
 });
